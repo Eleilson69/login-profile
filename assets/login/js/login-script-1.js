34488470da7104c3dadcabaa38bdaad473085dc0
ELEILSON SANTOS <105988025+Eleilson69@users.noreply.github.com>
2022-08-09 11:09:53 -0300

    update

---
+++ assets/login/js/login-script-1.js
@@ -0,0 +1,115 @@
+const email = document.querySelector('#email')
+const passwordInput = document.querySelector('#password')
+const eyeoutline = document.querySelector('.eye-svg')
+const check = document.querySelector('.eye-svg')
+const enter = document.querySelector('#bnt')
+const form = document.querySelector('.card-login')
+
+let usuario = [
+    { email: 'Elleylson.santtos.7@gmail.com', senha: 'Elleylson69' },
+    { email: 'maxta.gamer@gmail.com', senha: 'MxtTrap22' }
+]
+const emailValido = () => email.value
+const senhaValida = () => passwordInput.value
+
+
+const validateInputPassword = () => passwordInput.value.trim().length > 0;
+const validateInputEmail = () => email.value.trim().length > 0;
+
+const keyUpDisplayEye = () => {
+
+    check.classList.add('display-eye-svg')
+
+    if (passwordInput.value == "") {
+        check.classList.remove('display-eye-svg')
+    }
+
+}
+
+
+const emailErro = () => {
+    const InputEmailIsValide = validateInputEmail()
+
+    if (!InputEmailIsValide) {
+        return email.classList.add('error');
+
+
+    }
+}
+const removeEmailErro = () => {
+    const InputEmailIsValide = validateInputEmail()
+
+    if (InputEmailIsValide) {
+        email.classList.remove('error');
+
+    }
+}
+
+const passwordErro = () => {
+
+    const InputPasswordIsValide = validateInputPassword()
+
+    if (!InputPasswordIsValide) {
+        passwordInput.classList.add('error');
+
+    }
+
+}
+
+const removePasswordErro = () => {
+
+    const InputPasswordIsValide = validateInputPassword()
+
+    if (InputPasswordIsValide) {
+        return passwordInput.classList.remove('error')
+    }
+}
+
+enter.addEventListener('click', () => passwordErro())
+passwordInput.addEventListener('change', () => removePasswordErro())
+
+enter.addEventListener('click', () => emailErro())
+email.addEventListener('change', () => removeEmailErro())
+
+enter.addEventListener('click', () => enterProfile())
+
+const eyeclic = () => {
+
+    let passwordTypeispassword = passwordInput.type == "password"
+
+    if (passwordTypeispassword) {
+        showPassword()
+    } else {
+        //ação
+        hidePassword()
+    }
+
+}
+
+const showPassword = () => {
+    passwordInput.setAttribute("type", "text", )
+    eyeoutline.setAttribute("src", "svgs/eye-outline.svg")
+
+}
+
+const hidePassword = () => {
+    passwordInput.setAttribute("type", "password")
+    eyeoutline.setAttribute("src", "svgs/eye-off.svg")
+
+}
+
+
+
+form.addEventListener('submit', Event => {
+    Event.preventDefault()
+
+    const emailIsvalido = emailValido()
+    const senhaIsValida = senhaValida()
+
+    if (emailIsvalido == usuario[0].email && senhaIsValida == usuario[0].senha) {
+        return location = "https://eleilson69.github.io/profile/"
+    }
+})
+
+check.addEventListener('click', () => eyeclic())
+passwordInput.addEventListener('keyup', () => keyUpDisplayEye())
